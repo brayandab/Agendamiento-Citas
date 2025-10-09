312c07947ab49ff7970997b1dd5262b1adfe283f
Brayan Banguera <brayandavidbanguera@gmail.com>
2025-10-09 02:03:25 -0500

    limpiar

--- usuarios/src/main/java/com/eps/usuarios/repository/DoctorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.eps.usuarios.repository;
-
-import com.eps.usuarios.models.Doctor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DoctorRepository extends JpaRepository <Doctor, Long> {
-
-}
--- usuarios/src/main/java/com/eps/usuarios/repository/PacienteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.eps.usuarios.repository;
-
-import com.eps.usuarios.models.Paciente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PacienteRepository extends JpaRepository <Paciente,Long> {
-}
